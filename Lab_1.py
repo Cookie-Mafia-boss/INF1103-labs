e973c9427394664e3eaeab16be978ac7df9d3e02
MingYuen <2602969@sit.singaporetech.edu.sg>
2026-09-04 10:48:11 +0800

    First Post

---
+++ Lab_1.py
@@ -0,0 +1,51 @@
+#ACTIVTY 1
+print("===============")
+print("Welcome here!")
+print("This is my first post")
+print("===============")
+
+#ACTIVITY 2 
+username = "Im_am_a_SITstudent"
+bio = "Student"
+followers = 67
+
+
+#ACTIVITY 3
+followers += 6767
+print("Day 1:", followers)
+
+followers += 6700
+print("Day 2:", followers)
+
+followers -= 5900
+print("Day 3:", followers)
+
+#This will print the username
+print("Username:", username)
+
+#This will print the bio
+print("Bio:", bio)
+
+#This will print the amount of followers
+print("Followers:", followers)
+
+
+#ACTIVITY 4
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Catogory: ")
+
+
+print("====================")
+print("\nInstagram Profile")
+print("====================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category:", category)
+
+#git commit acts as a save point so i can go back to this file if i mess up later on, 
+#meaning i can go back to this version 
+
+#git commit -m "First Post" is just a brief note of what changes were made in this 
+#specific point of time
+
